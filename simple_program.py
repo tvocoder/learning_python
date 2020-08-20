6856f3ab97b288ff3c24dbc3a9315cb06a915b4c
tvocoder <42848470+tvocoder@users.noreply.github.com>
2020-08-20 13:30:38 -0500

    hello world notes

--- simple_program.py
+++ simple_program.py
@@ -1,1 +1,13 @@
 print("hello world")
+# w3schools.com
+# python runs on an interpreter system, meaning that code can executed as soon as it is written.
+# This means that prototyping can be very quick.
+
+# Python can be treated in a procedural way, an object-orientated way or a functional way.
+
+# Python uses new lines to complete a command, as opposed to other programming languages which often use
+# simicolons or parentheses
+
+# Python relies on indentation, using whitespace, to define scope.
+# Scope of loops, functions, and classes.
+# Other programming languages often use curly-brackets for this purpose.
