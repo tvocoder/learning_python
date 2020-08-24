85faa0bdb59eded9325ba412b1b35a66f0862ee1
tvocoder <42848470+tvocoder@users.noreply.github.com>
2020-08-24 17:54:53 -0500

    list_v0

---
+++ python_list.py
@@ -0,0 +1,36 @@
+# Lists are mutable ordered and indexed collections of objects.
+# The items of a list are arbitrary Python Objects.
+# Lists are formed by placing comma-separated list of expressions in square brackets.
+# (Note that there are no special cases needed to for lists of length 0 or 1)
+
+print("---= Constructors =---")
+print("-- list() --")
+print("-- [] List Comprehensions --")
+print("-- Literal Syntax --")
+
+print("---= Methods =---")
+print("--= Adding Elements =--")
+print("-- insert() --")
+print("-- append() --")
+print("-- extend() --")
+
+print("--= Deleting =--")
+print("-- remove() --")
+print("-- pop() --")
+
+print("--= Information =--")
+print("-- index() --")
+print("-- count() --")
+
+print("--= Modifying =--")
+print("-- sort() --")
+print("-- reverse() --")
+
+print("---= Functions =---")
+print("-- cmp() --")
+
+print("---= Operators =---")
+print("-- [](index operator) --")
+print("-- [:](slicing) --")
+print("-- +(concatention) --")
+print("-- *(multiple concatenation) --")
