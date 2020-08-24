471f5f49ad287cfe8071d81d2e270b9158e64a50
tvocoder <42848470+tvocoder@users.noreply.github.com>
2020-08-24 17:58:59 -0500

    list_v0

--- python_list.py
+++ python_list.py
@@ -5,32 +5,161 @@
 
 print("---= Constructors =---")
 print("-- list() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- [] List Comprehensions --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- Literal Syntax --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 
 print("---= Methods =---")
 print("--= Adding Elements =--")
+
 print("-- insert() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- append() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- extend() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 
 print("--= Deleting =--")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- remove() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- pop() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 
 print("--= Information =--")
 print("-- index() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- count() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 
 print("--= Modifying =--")
 print("-- sort() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- reverse() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 
 print("---= Functions =---")
+
 print("-- cmp() --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 
 print("---= Operators =---")
+
 print("-- [](index operator) --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- [:](slicing) --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- +(concatention) --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
 print("-- *(multiple concatenation) --")
+# Description:
+# Syntax:
+# Return Value:
+# Remarks:
+
+# Example:
+print("-------------------------")
