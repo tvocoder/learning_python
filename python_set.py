baee46a51bdd25be7d8b9585ea1f7edb9116c91b
tvocoder <42848470+tvocoder@users.noreply.github.com>
2020-08-24 15:30:15 -0500

    set_v0

---
+++ python_set.py
@@ -0,0 +1,81 @@
+# Sets are mutable unordered collections of unique elements.
+# Common uses include membership testing, removing duplicates from a sequence, and computing standard math operations
+# -- sets such as intersection, union,  difference, and symettric difference.
+
+# Sets do not record element position of insertion. Accordingly, sets do not support indexing, slicing, or other
+# -- sequence-like behavior
+
+# Sets are implemented using dictionaries. They cannot contain mutable elements such as lists or dictionaries.
+# -- However, they can contain immutable collections.
+
+print("---= Constructors =---")
+print("--- set() ---")
+print("--- {} set comprehension ---")
+print("--- literal syntax ---")
+
+print("---= Methods =---")
+
+print("--= Adding Elements =--")
+print("-- add() --")
+print("-- update() --")
+
+print("--= Deleting =--")
+print("-- discard() --")
+print("-- remove() --")
+print("-- pop() --")
+print("-- clear() --")
+
+print("--= Information =--")
+print("-- issuperset() --")
+print("-- issubset() --")
+print("-- isdisjoint() --")
+
+print("--= Set Operations =--")
+print("-- difference() --")
+print("-- intersection() --")
+print("-- symmetric_difference() --")
+print("-- union() --")
+
+print("--= Set Operations Assignment =--")
+print("-- difference_update() --")
+print("-- intersection_update() --")
+print("-- symmetric_difference_update() --")
+
+print("--= Copying =--")
+print("-- copy() --")
+
+print("---= Set Operators =---")
+print("--= Adding Elements =--")
+print("-- |=(update) --")
+
+print("--= Relational Operators =--")
+print("-- ==(is equal) --")
+print("-- !=(is not equal) --")
+print("-- <=(issubset) --")
+print("-- <(issubset proper) --")
+print("-- >=(issuperset) --")
+print("-- >(issuperset proper) --")
+
+print("--= Set Operations =--")
+print("** -(difference) **")
+print("-- &(intersection) --")
+print("-- ^(symmetric difference) --")
+print("-- |(union) --")
+
+print("--= Set Operations Assignment =--")
+print("** -=(difference_update) **")
+print("-- &=(intersection_update) --")
+print("-- ^=(symmetric_difference_update) --")
+
+print("---= Functions =---")
+print("-- len() --")
+print("-- min() --")
+print("-- max() --")
+print("-- sum() --")
+print("-- sorted() --")
+print("-- reversed() --")
+print("-- all() --")
+print("-- any() --")
+print("-- enumerate() --")
+print("-- zip() --")
+
