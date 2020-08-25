7d4e366ccf98d9fb7eee5868fa8b14a5b9744bb2
tvocoder <42848470+tvocoder@users.noreply.github.com>
2020-08-25 15:50:45 -0500

    file_rough_draft_v1

--- python_file.py
+++ python_file.py
@@ -18,18 +18,90 @@
 print("---= Constructors =---")
 
 print("-- file() --")
-# Description:
-# Syntax: unichr(number)
+# Description: Returns a file object.
+# Syntax: file(name[,model[,buffering]])
+# -- name: Optional. Full filepath.
+
+# -- mode: Optional. Possible values:
+# r: opening for reading (default)
+# w: open for writing, truncating the file first
+# a: open for writing, appending to the end of the file if it exists
+# b: binary mode
+# t: text mode (default)
+# +: open a disk file for updating (reading and writing)
+# U: universal newlines mode (for backwards compatibility; should not be used in new code)
+
+# -- buffering: Optional. Possible Values:
+# 0: to switch buffering off (only allowed in binary mode)
+# 1: to select line buffering (only usable in text mode)
+# int > 1: to indicate the size of a fixed-size chunk buffer.
+
 # Time Complexity:
-# Remarks:
+# Remarks: When opening a file, it's preferable to use open() instead of invoking this constructor directly.
+# -- File is more suited to type testing (for example, writing isinstance(f, file)).
 
 # Example:
+alice = open(r'alice_in_woderland.txt', 'r+')
+print(alice.readline())
 print("*************************")
+
 print("-- open() --")
-# Description:
-# Syntax: unichr(number)
+# Description: Opens a file returning a file object.
+# Syntax: open(name[,mode[,buffering]])
+# -- name: Optional. Full filepath.
+# *************************
+# -- mode: Optional. Possible Values:
+# r: open for reading (default)
+# w: open for writing, truncating the file first
+# a: open for writing, appending to the end of the file if it exists
+# b: binary mode
+# t: text mode (default)
+# +: open a disk file for updating (reading and writing)
+# U: universal newlines mode (for backwards compatibility, should not be used in new code)
+
+# The most commonly-used values of mode are
+# r: reading. If mode is omitted. Default
+# w: writing (truncating the file if it already exists)
+# a: (Unix systems all writes append to the end of the file regardless of the seek position)
+
+# The default is to use text mode, which may convert 'n' characters to a platform-specific representation
+# -- on writing and back on reading.
+# Thus, when opening a binary file, you should append 'b' to the mode value to open the file in binary mode,
+# -- which will improve portability.
+# Appending 'b' is useful even on systems that don't treat binary and text files differently.
+
+# Modes 'r+', 'w+', and 'a+' open the file for updating (note that 'w+' truncates the file).
+# Append 'b' to the mode to open the file in binary mode, on systems that differentiate between binary and text files;
+# -- on systems that don't have this distinction, adding the 'b' has no effect.
+# *************************
+# -- buffering: Optional. Possible values:
+# 0: to switch buffering off (only allowed in binary mode)
+# 1: to select line buffering (only usable in text mode)
+# int > 1: to indicate the size of a fixed-size chunk buffer
+
+# The optional buffering argument specifies the file's desired buffer size:
+# 0: Unbuffered
+# 1: Buffered
+# int > 1: Buffered, any other positive value means use a buffer of (approximately) that size (in bytes).
+# A negative buffering means to use the system default, which is usually line buffered for tty devices and
+# -- full buffered for other files. If omitted, the system default is used.
+# *************************
+
 # Time Complexity:
-# Remarks:
+
+# Remarks: In addition to the standard fopen() values mode may be 'U' or 'rU'.
+# Python is usually built with universal newlines support;
+# 'U': opens the file as a text file, but lines may be terminated by any of the following:
+# -- Unix end-of-line convention: 'n'
+# -- Macintosh convention: 'r'
+# -- Windows convention: 'rn'
+# All of these external representations are seen as 'n' by the Python program.
+# If Python is built without universal newlines support a mode with 'U' is the same as normal text mode.
+# Note that file objects so opened also have an attribute called newlines which has a value of None.
+# -- (If no newlines have yet been seen), -- 'n' -- 'r' -- 'rn', or a tuble containing all the newline types seen.
+# Python enforces that the mode, after stripping 'U', begins with 'r', 'w', or 'a'.
+# Python provides many file handling modules including
+# -- fileinput --  os -- os.path -- tempfile -- shutil
 
 # Example:
 print("*************************")
