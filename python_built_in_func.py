401980c856900b625373a9c883957d6488b4f501
tvocoder <42848470+tvocoder@users.noreply.github.com>
2020-08-26 13:55:06 -0500

    python_built_in_v1

--- python_built_in_func.py
+++ python_built_in_func.py
@@ -3,38 +3,197 @@ print("---= Built-In Functions =---")
 print("--= Functional Programming =--")
 print("-- map() --")
 print("-- filter() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- reduce() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
 
 print("--= Numeric Types Conversions and Constructors =--")
 print("-- bool() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- int() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- long() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- float() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- complex() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= Numeric Types Conversions =--")
 print("-- bin() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- oct() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- hex() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= Arithmetic =--")
 print("-- abs() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- pow() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- round() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- divmod() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= String Conversions =--")
 print("-- chr() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- ord() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- unichr() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- format() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- repr() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= Sequences Constructors =--")
 print("-- str() --")
 print("-- unicode() --")
 print("-- list() --")
 print("-- tuple() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- bytearray() --")
 print("-- buffer() --")
 print("-- memoryview() --")
@@ -43,8 +202,32 @@ print("-- xrange() --")
 
 print("--= Mappings Constructors =--")
 print("-- dict() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- set() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- frozenset() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= Operating on Containers =--")
 print("-- enumerate() --")
@@ -57,7 +240,22 @@ print("-- slice() --")
 
 print("--= Iterators =--")
 print("-- iter() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- next() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
 
 print("--= Comparisons =--")
 print("-- cmp() --")
@@ -68,7 +266,23 @@ print("-- any() --")
 
 print("--= Identity =--")
 print("-- hash() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- id() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= File Objects Constructors =--")
 print("-- file() --")
@@ -76,25 +290,169 @@ print("-- open() --")
 
 print("--= Operating on Containers =--")
 print("-- classmethod() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- property() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- staticmethod() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- super() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- setattr() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- getattr() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- delattr() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- hasattr() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- isinstance() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- issubclass() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- vars() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- dir() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- type(1)() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- type(2)() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= Information =--")
 print("-- callable() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- globals() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- locals() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- help() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 
 print("--= System =--")
 print("-- __import__ --")
@@ -103,12 +461,59 @@ print("-- compile() --")
 print("-- execfile() --")
 print("-- eval() --")
 print("-- input() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- intern() --")
 print("-- print() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- raw_input() --")
 
 print("--= Input =--")
 print("-- object() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- apply() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- basestring() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
+
 print("-- coerce() --")
+# Description:
+# Syntax:
+# Return Value:
+# Note:
+
+# Example:
+print("*************************")
